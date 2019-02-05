f2952a7f122515d637f06aaae06c47de091e689a
fathisye <47281515+fathisye@users.noreply.github.com>
2019-02-06 01:56:06 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("542328854199664661")
+setInterval(function() {
+channel.send(`spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspamspamcraditspam
+spamcraditspamspamcraditspam`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
